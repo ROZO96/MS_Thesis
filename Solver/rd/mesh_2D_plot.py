1572ad5a5ad551d22e249f61f869d908e8aa04f3
Daniel Rozo <s2344208@ed.ac.uk>
2023-06-16 00:24:31 +0100

    Error correction and two mesh are shown one with the python triangulation and another with the Cgal one

--- Solver/rd/mesh_2D_plot.py
+++ Solver/rd/mesh_2D_plot.py
@@ -20,16 +20,27 @@ for string in lines[3:n_points+3]:
 points=np.asarray(points)
 val_x,val_y=points[:,0].astype(float),points[:,1].astype(float)
 
+
 triangles = []
 for string in lines[n_points+5:n_points+5+n_triangles]:
     triangles.append(string.split())
 triangles=np.asarray(triangles).astype(int)
+print(triangles)
 
 
 fig, ax = plt.subplots()
-triang=mtri.Triangulation(val_x, val_y, triangles)
-plt.triplot(triang,'o-',markersize=1,linewidth=0.1)
-plt.xlim(xmin,xmax)
-plt.ylim(ymin,ymax)
+#triang=mtri.Triangulation(val_x, val_y, triangles)
+triang= mtri.Triangulation(val_x, val_y)
+print(triang.triangles)
+ax.triplot(val_x, val_y,triangles,'o-',markersize=1,linewidth=0.5)
+ax.triplot(triang,'o-',markersize=1,linewidth=0.5)
+"""
+for i in range(n_triangles):
+    ax.plot((val_x[int(triangles[i,0])],val_x[int(triangles[i,1])]),(val_y[int(triangles[i,0])],val_y[int(triangles[i,1])]),color='red')
+    ax.plot((val_x[int(triangles[i,0])],val_x[int(triangles[i,2])]),(val_y[int(triangles[i,0])],val_y[int(triangles[i,2])]),color='red')
+    ax.plot((val_x[int(triangles[i,1])],val_x[int(triangles[i,2])]),(val_xy[int(triangles[i,1])],val_y[int(triangles[i,2])]),color='red')
+"""
+ax.set_xlim(float(xmin),float(xmax))
+ax.set_ylim(float(ymin),float(ymax))
 
 plt.show()
